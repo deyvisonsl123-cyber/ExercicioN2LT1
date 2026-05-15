87f21ba57a0ad7ae876413f52a3d12fd5b843610
deyvisonsl123-cyber <deyvisonsl123@gmail.com>
2026-05-15 19:41:36 -0400

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    // Adicione seu código aqui
-    
+void trocar(int *a, int *b){
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+    int x, y;
+
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+
+    printf("Digite o valor de y: ");
+    scanf("%d", &y);
+
+    trocar(&x, &y);
+
+    printf("\nValores trocados: \n");
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+
     return 0;
 }
