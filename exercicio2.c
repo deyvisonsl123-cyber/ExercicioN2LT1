8b57663e51b49f6a3d5161e9521a62610ce8e97e
deyvisonsl123-cyber <deyvisonsl123@gmail.com>
2026-05-15 19:53:04 -0400

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    // Adicione seu código aqui
-    
+void encontrarMaiorMenor(int *array, int tamanho, int *maior, int*menor){
+    int i;
+
+    *maior = array[0];
+    *menor = array[0];
+
+    for(i = 1; i < tamanho; i++){
+        if(array[i] > *maior){
+            *maior = array[i];
+        }
+
+        if(array[i] < *menor){
+            *menor = array[i];
+        }    
+    }
+}
+
+int main(){
+    int numeros[5];
+    int i, maior, menor;
+
+    printf("Digite 5 números: \n");
+
+    for( i = 0; i < 5; i++){
+        scanf("%d", &numeros[i]);
+    }
+
+    encontrarMaiorMenor(numeros, 5, &maior, &menor);
+
+    printf("\nMaior elemento: %d\n", maior);
+    printf("Menor elemento: %d\n", menor);
+
     return 0;
 }
